0ac4cdf9eadf8901bb80a2c07325e94676dd9d00
ioComk <rw110407@gmail.com>
2019-05-18 14:39:03 +0900

    Update 窓関数を実装 ハン窓，ハミング窓，ブラックマン窓 デフォルト引数はハン窓

--- STFT/stft.py
+++ STFT/stft.py
@@ -5,22 +5,30 @@ import matplotlib as mpl
 import matplotlib.pyplot as plt
 from scipy.fftpack import fft
 
-def stft(signal, frame, shift):
+
+def stft(signal, frame, shift, windowtype='hann'):
     begin = 0
     col = 0  # 配列に格納する際の列のインデックス
 
     mat = np.zeros((frame, int(len(y)/shift)))  # スペクトログラムを格納する行列，大体の大きさを確保しておく
+    if windowtype == 'hann':
+        window = np.hanning(frame)
+    elif windowtype == 'hamming':
+        window = np.hamming(frame)
+    elif windowtype == 'blackman':
+        window = np.blackman(frame)
 
     while frame <= len(y):
-        mat[:, col] = abs(fft(y[begin:frame])).T
+        mat[:, col] = abs(fft(y[begin:frame])).T*window
         # それぞれシフト長だけシフト
         begin += shift
         frame += shift
         col += 1
 
     mat = mat[:, :col]  # 余分な列を削除
-    mat = 20*np.log1p(mat[int(mat.shape[0]/2):int(mat.shape[0]), :])  # 下半分だけ抽出して利得を計算
-    
+    # 下半分だけ抽出して利得を計算
+    mat = 20*np.log1p(mat[int(mat.shape[0]/2):int(mat.shape[0]), :])
+
     return mat
 
 if __name__ == '__main__':
@@ -29,7 +37,7 @@ if __name__ == '__main__':
     # y = amplitude, sr = sample rate
     y, sr = sf.read(filename)
 
-    spectrogram = stft(y,1000,150)
+    spectrogram = stft(y, 1000, 150, 'hamming')
 
     plt.imshow(spectrogram, cmap="inferno")
     plt.colorbar()
