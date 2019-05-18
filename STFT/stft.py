1b6380dd86cc71fe9e98edd0a1ba53b2c4cacb49
ioComk <rw110407@gmail.com>
2019-05-18 12:12:03 +0900

    Update STFTを関数化

--- STFT/stft.py
+++ STFT/stft.py
@@ -5,6 +5,21 @@ import matplotlib as mpl
 import matplotlib.pyplot as plt
 from scipy.fftpack import fft
 
+def stft(signal, frame, shift):
+    begin = 0
+    col = 0
+    mat = np.zeros((frame, int(len(y)/shift)))
+
+    while frame <= len(y):
+        mat[:, col] = abs(fft(y[begin:frame])).T
+        begin += shift
+        frame += shift
+        col += 1
+
+    mat = mat[:, :col]
+    mat = 20*np.log1p(mat[int(mat.shape[0]/2):int(mat.shape[0]), :])
+    
+    return mat
 
 if __name__ == '__main__':
     filename = 'wav/s1.wav'
@@ -12,27 +27,8 @@ if __name__ == '__main__':
     # y = amplitude, sr = sample rate
     y, sr = sf.read(filename)
 
-    begin = 0
+    spectrogram = stft(y,1000,150)
 
-    frame = 1000
-    shift = 150  # シフト長
-    col = 0
-
-    totaltime = len(y)/sr
-    mat = np.zeros((frame, int(len(y)/shift)))  # 作成するスペクトログラム
-    
-    while frame <= len(y):
-        buf = abs(fft(y[begin:frame]))
-        mat[:, col] = buf.T
-        begin += shift
-        frame += shift
-        col += 1
-    
-    mat = mat[:, :col]  # 余分な列を削る
-    
-    # 下半分を抽出し利得を計算
-    mat_map = 20*np.log1p(mat[int(mat.shape[0]/2):int(mat.shape[0]), :])
-    
-    plt.imshow(mat_map, cmap="inferno")
+    plt.imshow(spectrogram, cmap="inferno")
     plt.colorbar()
     plt.show()
